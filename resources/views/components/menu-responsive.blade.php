a95b0dead4e1aa12ce37056e455a35b0d87e2115
stephenjude <stephenjudesuccess@gmail.com>
2019-12-02 10:58:06 +0100

    remove unused curly brackets

--- resources/views/components/menu-responsive.blade.php
+++ resources/views/components/menu-responsive.blade.php
@@ -4,7 +4,7 @@
             <a
                 title="About {{ $meta['title'] ?? config('services.meta.site_name') }}"
                 href="{{url('about')}}"
-                class="nav-menu__item hover:text-blue-500 active text-blue }}"
+                class="nav-menu__item hover:text-blue-500 active text-blue"
             >About</a>
         </li>
     </ul>
